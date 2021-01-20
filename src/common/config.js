7cb24ad2a43b732b3ae097b7080c998b3b7d3e08
yy1111yang <yy1111yang@naver.com>
2021-01-20 18:02:44 +0900

    first commit

--- src/common/config.js
+++ src/common/config.js
@@ -1,2 +1,3 @@
-export const API_URL = "https://conduit.productionready.io/api";
+export const API_URL = "http://localhost:3000/api";
+// export const API_URL = "https://conduit.productionready.io/api";
 export default API_URL;
