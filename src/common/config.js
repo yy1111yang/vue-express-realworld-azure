a8f701486606f23e787238c9fdc4387eed7cde1e
yy1111yang <yy1111yang@naver.com>
2021-01-22 05:34:45 +0000

    add dockerfile, k8s

--- src/common/config.js
+++ src/common/config.js
@@ -1,3 +1,2 @@
-export const API_URL = "http://localhost:3000/api";
-// export const API_URL = "https://conduit.productionready.io/api";
+export const API_URL = "http://<>:3000/api";
 export default API_URL;
